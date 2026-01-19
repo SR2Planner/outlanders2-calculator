4aadafb490e84d03624db25b4448de8f9bea853a
SR2Planner <slimerancher2planner@gmail.com>
2026-01-19 22:09:31 +0100

    add some of the missing buildings

--- data.js
+++ data.js
@@ -7,7 +7,9 @@ const RESOURCES = [
     { id: 'stone', name: 'Stone', source: 'Produced at Quarry or Stone Mine (6 logs → 4 stone in Stone Mine).' },
     { id: 'clay', name: 'Clay', source: 'Produced by Clay Pit.' },
     { id: 'bricks', name: 'Bricks', source: 'Produced by Brickyard (1 stick + 8 clay → 8 bricks).' },
-    { id: 'reeds', name: 'Reeds', source: 'Gathered by Reed Harvester.' }
+    { id: 'reeds', name: 'Reeds', source: 'Gathered by Reed Harvester.' },
+    { id: 'glass', name: 'Glass', source: 'Produced from.' }
+
   ];
   
   // Categories
@@ -23,9 +25,10 @@ const RESOURCES = [
 
   const BUILDINGS = [
     // construction
-    { id: 'lumberhack', name: 'Lumberjacks\' Base', category: 'construction', cost: { sticks: 6 } },
+    { id: 'lumberjack', name: 'Lumberjacks\' Base', category: 'construction', cost: { sticks: 6 } },
     { id: 'clay', name: 'Clay Pit', category: 'construction', cost: { sticks: 10 } },
     { id: 'quarry', name: 'Quarry', category: 'construction', cost: { logs: 6 } },
+    { id: 'reedHarvester', name: 'Reed Harvester', category: 'construction', cost: { sticks: 6 } },
     { id: 'sawmill', name: 'Sawmill', category: 'construction', cost: { logs: 9 } },
     { id: 'brickyar', name: 'Brickyard', category: 'construction', cost: { sticks: 9 } },
     { id: 'foresters', name: 'Foresters\' Hub', category: 'construction', cost: { planks: 10 } },
@@ -35,6 +38,7 @@ const RESOURCES = [
   
     // food
     { id: 'forager', name: 'Forager\'s Hut', category: 'food', cost: { sticks: 9 } },
+    { id: 'trapper', name: 'Trapper\'s Hut', category: 'food', cost: { sticks: 8, logs: 4 } },
     { id: 'farm', name: 'Farm', category: 'food', cost: { sticks: 4, clay: 10 } },
     { id: 'orchard', name: 'Orchard', category: 'food', cost: { planks: 10 } },
     { id: 'kitchen', name: 'Open Kitchen', category: 'food', cost: { sticks: 6, clay: 8 } },
@@ -42,15 +46,23 @@ const RESOURCES = [
     { id: 'windmill', name: 'Windmill', category: 'food', cost: { clay: 15, stone: 5 } },
     { id: 'bakery', name: 'Bakery', category: 'food', cost: { clay: 9, stone: 4  } },
     { id: 'cookie', name: 'Cookie Shop', category: 'food', cost: { planks: 3, bricks: 8  } },
+    { id: 'greenhouse', name: 'Green House', category: 'food', cost: { glass: 6, stone: 6  } },
+    { id: 'cheesery', name: 'Cheesery', category: 'food', cost: { logs: 10, bricks: 8  } },
     { id: 'fondue', name: 'Fondue Bar', category: 'food', cost: { logs: 10, bricks: 8  } },
     { id: 'driedmeat', name: 'Dried Meat Shop', category: 'food', cost: { logs: 10 } },
     { id: 'dock', name: 'Fishing Dock', category: 'food', cost: { planks: 18 } },
+    { id: 'nopaleroHut', name: 'Nopalero\'s Hut', category: 'food', cost: { logs: 5 } },
+    { id: 'desertCook', name: 'Desert Cookhouse', category: 'food', cost: { sticks: 6, clay: 8 } },
+    { id: 'oasis', name: 'Oasis', category: 'food', cost: { planks: 10 } },
 
     // housing
     { id: 'basicH', name: 'Basic House', category: 'housing', cost: { logs: 6 } },
     { id: 'plankH', name: 'Plank House', category: 'housing', cost: { planks: 6 } },
     { id: 'brickH', name: 'Brick House', category: 'housing', cost: { bricks: 12} },
     { id: 'twoH', name: 'Two-Story House', category: 'housing', cost: { bricks: 8, stone: 8 } },
+    { id: 'winterH', name: 'Winter Cabin', category: 'housing', cost: { planks: 3, stone: 3 } },
+    { id: 'tropicalH', name: 'Tropical Shelter', category: 'housing', cost: { logs: 4, reeds: 8 } },
+    { id: 'desertH', name: 'Desert Home', category: 'housing', cost: { clay: 8 } },
  
     // infrastructure
     { id: 'roadworks', name: 'Roadworks Office', category: 'infrastructure', cost: { logs: 6 } },
@@ -65,15 +77,22 @@ const RESOURCES = [
     { id: 'plaza', name: 'Community Plaza', category: 'lifestyle', cost: { planks: 57} },
     { id: 'zen', name: 'Zen Garden', category: 'lifestyle', cost: { stone: 6 } },
     { id: 'fitness', name: 'Fitness Park', category: 'lifestyle', cost: { planks: 10, stone: 6 } },
+    { id: 'bonfire', name: 'Bonfire', category: 'lifestyle', cost: { logs: 12 } },
+    { id: 'icepop', name: 'Ice Pop Stand', category: 'lifestyle', cost: { logs: 9, clay: 6 } },
     { id: 'brewery', name: 'Brewery', category: 'lifestyle', cost: { stone: 4, planks: 15 } },
     { id: 'smoothie', name: 'Smoothie Stand', category: 'lifestyle', cost: { logs: 4, planks: 6 } },
     { id: 'paperMill', name: 'Paper Mill', category: 'lifestyle', cost: { planks: 14 } },
     { id: 'press', name: 'Press', category: 'lifestyle', cost: { bricks: 24 } },
     { id: 'bookWorkshop', name: 'Book Workshop', category: 'lifestyle', cost: { stone: 12, planks: 8 } },
+    { id: 'canteen', name: 'Canteen Factory', category: 'lifestyle', cost: { bricks: 16, planks: 6 } },
+    { id: 'swimming', name: 'Swimming Pier', category: 'lifestyle', cost: { stone: 16, planks: 8 } },
     { id: 'bottleFactory', name: 'Bottle Factory', category: 'lifestyle', cost: { bricks: 6, clay: 6 } },
     { id: 'winery', name: 'Winery', category: 'lifestyle', cost: { stone: 10, planks: 8 } },
+    { id: 'tailor', name: 'Tailor', category: 'lifestyle', cost: { bricks: 12, } },
+    { id: 'pub', name: 'Winter Pub', category: 'lifestyle', cost: { logs: 10, stone: 8 } },
     { id: 'craftshop', name: 'Pendant Craftshop', category: 'lifestyle', cost: { stone: 4, bricks: 8 } },
     { id: 'luthierie', name: 'Luthierie', category: 'lifestyle', cost: { logs: 14 } },
+    { id: 'well', name: 'Well', category: 'lifestyle', cost: { logs: 8, stone: 12 } },
 
     // decorations
     { id: 'decorations_a', name: 'Calla Lilly Planter', category: 'decorations', cost: { stone: 2 } },
